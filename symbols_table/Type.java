cb8bab01df90f7283d239d7d941bff11adc41065
jp-huppes <jpharenale@gmail.com>
2026-05-20 16:47:20 -0400

    feat: adding class Type

--- symbols_table/Type.java
+++ symbols_table/Type.java
@@ -1,5 +1,28 @@
 package symbols_table;
 
 public class Type {
-    
+    private String name;
+
+    // Construtor
+    Type() {
+    };
+
+    Type(String name) {
+        this.name = name;
+    }
+
+    // Copia
+    Type(Type x) {
+        this.name = x.name;
+    }
+
+    // Métodos
+    public String id() {
+        return name;
+    }
+
+    public Bool isValid() {
+
+    }
+
 }
