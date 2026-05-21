2432f7f79510ea0d6cbdfe30b508eb983e4180be
jp-huppes <jpharenale@gmail.com>
2026-05-21 12:40:26 -0400

    feat: go find something new to do Paola Campos, let me commit my stuff in peace. Well, this is the update of the class Symbol

--- symbols_table/Symbol.java
+++ symbols_table/Symbol.java
@@ -1,39 +1,95 @@
 package symbols_table;
 
-import symbols_table.scope.*;
-import symbols_table.symbols.*;;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Symbol {
-    public String name;
-    public Modifier[] modifiers;
+    private String name;
+    private Set<Modifier> modifiers;
 
+    // Construtor Default; Inicialização da coleção vazia (evita
+    // NullPointerException)
     public Symbol() {
+        this.modifiers = new HashSet<>();
     };
 
-    public Symbol(String name, Modifier[] modifiers) {
+    // Construtor
+    public Symbol(String name, Set<Modifier> modifiers) {
         this.name = name;
-        for (int i = 0; i < modifiers.length; ++i)
-            this.modifiers[i] = modifiers[i];
+        if (modifiers != null)
+            this.modifiers = modifiers;
+        // Caso aconteça um erro, ou algo inesperado, a coleção é inciada vazia
+        else
+            this.modifiers = new HashSet<>();
     }
 
+    // Cópia
     public Symbol(Symbol symbol) {
-        this.name = symbol.name;
-        for (int i = 0; i < modifiers.length; ++i)
-            this.modifiers[i] = symbol.modifiers[i];
+        if (symbol != null) {
+            this.name = symbol.name;
+            this.modifiers = new HashSet<>(symbol.modifiers);
+        } else
+            this.modifiers = new HashSet<>();
+    }
+
+    // Acessos ao atributos privados
+    public String getName() {
+        return this.name;
+    }
+
+    public Set<Modifier> getModifiers() {
+        return Collections.unmodifiableSet(this.modifiers);
+    }
+
+    // Método de adição de novos modificadores
+    public void addModifier(Modifier modifier) {
+        if (modifier != null)
+            this.modifiers.add(modifier);
+    }
+
+    // Verificadores
+    public boolean hasModifier(Modifier modifier) {
+        return this.modifiers.contains(modifier);
     }
 
     public boolean isValidName(String name) {
-        return !(name.contains(" "));
-    }
-
-    public boolean isValidModifier() {
-        int i;
-        for (i = 0; i < modifiers.length; ++i) {
-            if (true)
-                return false;
+        if (name != null) {
+            if (!name.contains(" ") || !name.contains(".")) {
+                return true;
+            }
         }
-
-        return true;
+        return false;
     }
 
-}
+    // Verificadores para Scope
+    public boolean hasPrivate() {
+        return hasModifier(Modifier.PRIVATE);
+    }
+
+    public boolean hasProtected() {
+        return hasModifier(Modifier.PROTECTED);
+    }
+
+    public boolean hasPublic() {
+        return hasModifier(Modifier.PUBLIC);
+    }
+
+    public boolean hasStatic() {
+        return hasModifier(Modifier.STATIC);
+    }
+
+    public boolean hasAbstract() {
+        return hasModifier(Modifier.ABSTRACT);
+    }
+
+    public boolean hasFinal() {
+        return hasModifier(Modifier.FINAL);
+    }
+
+    @Override
+    public String toString() {
+        return "Symbol{name =" + name + "', modifiers=" + modifiers + "}";
+    }
+
+} // Symbol
