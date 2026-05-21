3288dc709b4ce345829c0894a208ed8e56b517ae
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-21 10:55:30 -0400

    feat: adding ClassSymbol class

--- symbols_table/symbols/ClassSymbol.java
+++ symbols_table/symbols/ClassSymbol.java
@@ -6,11 +6,49 @@ import symbols_table.*;
 
 import java.util.TreeMap;
 
-public class ClassSymbol extends Symbol
-        implements Scope {
+public class ClassSymbol extends Symbol implements Scope {
+    // Atributos
     public ClassSymbol superclass;
 
-    private TreeMap<String, Atribute> atributes;
+    private TreeMap<String, Attribute> attributes;
     private TreeMap<String, Method> methods;
     private TreeMap<String, Constructor> constructors;
+
+    // Construtores
+    public ClassSymbol() {
+        this.attributes = new TreeMap<>();
+        this.methods = new TreeMap<>();
+        this.constructors = new TreeMap<>();
+    }
+
+    public ClassSymbol(String name) {
+        this.name = name;
+
+        this.attributes = new TreeMap<>();
+        this.methods = new TreeMap<>();
+        this.constructors = new TreeMap<>();
+    }
+
+    public ClassSymbol(String name, ClassSymbol superclass) {
+        this.name = name;
+        this.superclass = superclass;
+
+        this.attributes = new TreeMap<>();
+        this.methods = new TreeMap<>();
+        this.constructors = new TreeMap<>();
+    }
+
+    Attribute getAttribute(String attributeName) {
+        return attributes.get(attributeName);
+    }
+
+    Method getMethod(String methodName) {
+        return methods.get(methodName);
+    }
+
+    Constructor getConstructor(String constructorName) {
+        return constructors.get(constructorName);
+    }
+
+
 }
