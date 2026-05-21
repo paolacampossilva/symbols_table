69a391dd2010cb83fe0962a5de2822778215cd46
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-21 18:13:55 -0400

    feat: adding class GlobalScope

--- symbols_table/scope/GlobalScope.java
+++ symbols_table/scope/GlobalScope.java
@@ -7,11 +7,43 @@ import symbols_table.*;
 import java.util.*;
 
 /**
- * @author Paola Campos da Silca
+ * @author Paola Campos da Silva
  * @author João Pedro Huppes Arenales
  */
 
-public class GlobalScope
-        implements Scope {
+public class GlobalScope implements Scope {
+    // Atributos
     private TreeMap<String, ClassSymbol> classes;
+
+    // Construtores
+    public GlobalScope() {
+        this.classes = new TreeMap<>();
+    }
+
+    // Métodos
+    public ClassSymbol getClass(String name) {
+        return classes.get(name);
+    }
+
+    public void addClass(ClassSymbol classSymbol) {
+        if (this.getClass(classSymbol.getName()) == null)
+            classes.put(classSymbol.getName(), classSymbol);
+    }
+
+    @Override
+    public Scope getParent() {
+        return null; 
+    }
+
+    @Override
+    public Symbol search(String name) {
+        if (classes.containsKey(name)) {
+            return this.getClass(name);
+        }
+        
+        // 2. Como não há escopo pai para delegar a busca (ele é o topo da hierarquia),
+        // se o símbolo não foi encontrado até aqui, retornamos null garantindo 
+        // que o identificador não existe na tabela.
+        return null; 
+    }
 }
