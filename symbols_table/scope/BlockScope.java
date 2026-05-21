5db64bea39f392107a9b55bc5c252b7849484752
jp-huppes <jpharenale@gmail.com>
2026-05-21 19:34:15 -0400

    feat: built the classes: MethodScope & BlockScope

--- symbols_table/scope/BlockScope.java
+++ symbols_table/scope/BlockScope.java
@@ -4,6 +4,49 @@ import symbols_table.symbols.*;
 import symbols_table.*;
 import java.util.TreeMap;
 
+/**
+ * @author Paola Campos da Silva
+ * @author João Pedro Huppes Arenales
+ */
+
 public class BlockScope implements Scope {
-    private TreeMap<String, Variable> variables;
+    private final Scope parent;
+    private TreeMap<String, Symbol> symbols;
+
+    // Construtor
+    public BlockScope(Scope parent) {
+        this.parent = parent;
+        symbols = new TreeMap<>();
+    }
+
+    @Override
+    public Scope getParent() {
+        return parent;
+    }
+
+    @Override
+    public Symbol search(String name) {
+        if (symbols.containsKey(name))
+            return symbols.get(name);
+        else if (this.parent != null)
+            return this.parent.search(name);
+        else
+            return null;
+    }
+
+    @Override
+    public void define(Symbol symbol) throws DuplicateSymbolException, LogicalException {
+        if (symbol instanceof ClassSymbol)
+            throw new LogicalException(symbol.getClass().getSimpleName());
+        String name = symbol.getName();
+        if (symbols.containsKey(name))
+            throw new DuplicateSymbolException();
+
+        symbols.put(name, symbol);
+    }
+
+    @Override
+    public String toString() {
+        return "BlockScope" + symbols.keySet();
+    }
 }
--- symbols_table/scope/MethodScope.java
+++ symbols_table/scope/MethodScope.java
@@ -2,7 +2,53 @@ package symbols_table.scope;
 
 import symbols_table.symbols.*;
 import symbols_table.*;
+import java.util.TreeMap;
 
+/**
+ * @author Paola Campos da Silva
+ * @author João Pedro Huppes Arenales
+ */
 public class MethodScope implements Scope {
+    private final Scope parent;
+    private TreeMap<String, Symbol> symbols;
+
+    // Construtor
+    public MethodScope(Scope parent) {
+        this.parent = parent;
+        this.symbols = new TreeMap<>();
+    }
+
+    @Override
+    public Scope getParent() {
+        return parent;
+    }
+
+    @Override
+    public void define(Symbol symbol) throws DuplicateSymbolException, LogicalException {
+        if (symbol instanceof ClassSymbol)
+            throw new LogicalException(symbol.getClass().getSimpleName());
+
+        String name = symbol.getName();
+
+        if (symbols.containsKey(name))
+            throw new DuplicateSymbolException(name);
+
+        symbols.put(name, symbol);
+    }
+
+    @Override
+    public Symbol search(String name) {
+        if (symbols.containsKey(name))
+            return symbols.get(name);
+        else if (this.parent != null)
+            return this.parent.search(name);
+        else
+            return null;
+    }
+
+    @Override
+    public String toString() {
+        return "MethodScope" + symbols.keySet();
+    }
 
 }
