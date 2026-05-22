440a11cc6009fb95c25842c97d2f6b66a57a3904
jp-huppes <jpharenale@gmail.com>
2026-05-22 17:10:09 -0400

    style: better parameters' names

--- symbols_table/scope/LogicalException.java
+++ symbols_table/scope/LogicalException.java
@@ -4,4 +4,8 @@ public class LogicalException extends Exception {
     public LogicalException(String clazz) {
         super("Error: " + clazz + " is not acceptable as a parameter for this method.");
     }
+
+    public LogicalException(String scope, String symbol) {
+        super("Error: is not acceptable to add a " + symbol + " without a open " + scope);
+    }
 }
