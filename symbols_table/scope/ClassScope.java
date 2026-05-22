902017d51ad1d9b0501de025308d1e1b2d6912e3
jp-huppes <jpharenale@gmail.com>
2026-05-22 17:12:17 -0400

    feat: built the new class ClassScope, now there is no need to use the class ClassSymbol as a symbol & scope

--- symbols_table/scope/ClassScope.java
+++ symbols_table/scope/ClassScope.java
@@ -4,13 +4,14 @@ import symbols_table.symbols.*;
 
 import symbols_table.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeMap;
 
 public class ClassScope implements Scope {
     // Atributos
     private final Scope parent;
     private ClassSymbol classSymbol;
-
     private TreeMap<String, Attribute> attributes;
     private TreeMap<String, Method> methods;
     private TreeMap<String, Constructor> constructors;
@@ -26,6 +27,11 @@ public class ClassScope implements Scope {
     }
 
     // Métodos
+
+    public ClassSymbol getClassSymbol() {
+        return classSymbol;
+    }
+
     Attribute getAttribute(String attributeName) {
         return attributes.get(attributeName);
     }
@@ -34,20 +40,12 @@ public class ClassScope implements Scope {
         return methods.get(methodName);
     }
 
+    public List<Method> getAllMethods() {
+        return new ArrayList<>(methods.values());
+    }
+
     Constructor getConstructor(String constructorName) {
         return constructors.get(constructorName);
-    }
-
-    public void addAttribute(Attribute attribute) {
-        attributes.put(attribute.getName(), attribute);
-    }
-
-    public void addMethod(Method method) {
-        methods.put(method.getName(), method);
-    }
-
-    public void addConstructor(Constructor constructor) {
-        constructors.put(constructor.getName(), constructor);
     }
 
     @Override
@@ -56,22 +54,55 @@ public class ClassScope implements Scope {
     }
 
     @Override
-    public Symbol search(String name) {
-        if (attributes.containsKey(name))
-            return this.getAttribute(name);
-
-        if (methods.containsKey(name))
-            return this.getMethod(name);
-
-        if (constructors.containsKey(name))
-            return this.getConstructor(name);
-
-        Scope parent = getParent();
-        if (parent != null) {
-            return parent.search(name);
-        }
-
-        return null; // Símbolo não encontrado
+    public void define(Symbol symbol) throws DuplicateSymbolException, LogicalException {
+        if (symbol instanceof Attribute) {
+            if (attributes.containsKey(symbol.getName()))
+                throw new DuplicateSymbolException(symbol.getName());
+            attributes.put(symbol.getName(), (Attribute) symbol);
+        } else if (symbol instanceof Constructor) {
+            if (constructors.containsKey(symbol.getName()))
+                throw new DuplicateSymbolException(symbol.getName());
+            constructors.put(symbol.getName(), (Constructor) symbol);
+        } else if (symbol instanceof Method) {
+            Method m = (Method) symbol;
+            String signature = generateMethodSignature(m.getName(), m.getParameters());
+            if (methods.containsKey(signature))
+                throw new DuplicateSymbolException(signature);
+            methods.put(signature, m);
+        } else
+            throw new LogicalException("ClassScope", symbol.getClass().getName());
     }
 
+    @Override
+    public Symbol search(String name) {
+        if (attributes.containsKey(name))
+            return attributes.get(name);
+        if (constructors.containsKey(name))
+            return constructors.get(name);
+
+        for (Method m : methods.values())
+            if (m.getName().equals(name))
+                return m;
+
+        if (parent != null)
+            return parent.search(name);
+
+        return null;
+    }
+
+    public static String generateMethodSignature(String name, Parameter[] parameters) {
+        if (parameters == null || parameters.length == 0)
+            return name + "()";
+        StringBuilder sig = new StringBuilder(name + "(");
+        for (int i = 0; i < parameters.length; ++i) {
+            if (parameters[i].getType() != null)
+                sig.append(parameters[i].getType().toString());
+            else
+                sig.append("unknown");
+            if (i < parameters.length - 1)
+                sig.append(",");
+        }
+        sig.append(")");
+        return sig.toString();
+    }
 }
